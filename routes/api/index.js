949c0f2c9877f5139e9f11bf0dcc7525b458c849
Alexis Venegas <alexisvfilmstuff@protonmail.com>
2021-10-19 19:11:55 -0700

    added routes and tag data

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,11 +1,11 @@
-const router = require('express').Router()
+const router = require('express').Router();
+const categoryRoutes = require('./category-routes');
+const productRoutes = require('./product-routes');
+const tagRoutes = require('./tag-routes');
 
-router.use('/api', require('./category-routes.js'))
-router.use('/api', require('./product-routes.js'))
-router.use('/api', require('./tag-routes.js'))
+router.use('/categories', categoryRoutes);
+router.use('/products', productRoutes);
+router.use('/tags', tagRoutes);
 
-router.use((req, res) => {
-  res.send('<h1>Wrong Route!</h1>')
-})
+module.exports = router;
 
-module.exports = router
